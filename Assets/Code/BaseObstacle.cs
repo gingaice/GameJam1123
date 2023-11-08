c06d8f8d30424e4bcc2250adf8faef7d49038843
JonesCGD <80253281+gingaice@users.noreply.github.com>
2023-11-08 20:47:51 +0000

    some things

--- Assets/Code/BaseObstacle.cs
+++ Assets/Code/BaseObstacle.cs
@@ -11,6 +11,9 @@ public class BaseObstacle : MonoBehaviour
     private float Speed;
 
     // Start is called before the first frame update
+    [SerializeField]
+    private float timer;
+
 
     private void Awake()
     {
@@ -24,11 +27,18 @@ public class BaseObstacle : MonoBehaviour
         //transform.rotation = Quaternion.FromToRotation(transform.position, -player.position);
         transform.LookAt(player.position);
         rb.AddForce(transform.forward * Speed);
+
+        timer = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
--- Assets/Code/ObstacleSpawner.cs
+++ Assets/Code/ObstacleSpawner.cs
@@ -11,26 +11,24 @@ public class ObstacleSpawner : MonoBehaviour
     [SerializeField]
     GameObject asteroid;
 
-    //[SerializeField]
-    //float radius;
+    private float timer;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartSpawn();
+        //StartSpawn();
     }
-
-
-    //private void OnDrawGizmos()
-    //{
-    //    //this is to get how far the ai can see
-    //    Gizmos.color = Color.yellow;
-    //    Gizmos.DrawWireSphere(transform.position, radius);
-    //}
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+
+        if(timer <= 0)
+        {
+            StartSpawn();
+
+            timer = Random.Range(1,5);
+        }
     }
 
     void StartSpawn()
@@ -40,15 +38,12 @@ public class ObstacleSpawner : MonoBehaviour
         if (asteroid != null) 
         {
             //Instantiate(asteroid, );
-            for(int i = 0;i < 4;i++)
-            {
-                float offsetX = Random.Range(-bounds.extents.x, bounds.extents.x);
-                float offsetY = Random.Range(-bounds.extents.y, bounds.extents.y);
-                float offsetZ = Random.Range(-bounds.extents.z, bounds.extents.z);
-                //Instantiate(asteroid, Random.insideUnitSphere * radius + transform.position, Random.rotation);
-                GameObject newHazard = GameObject.Instantiate(asteroid);
-                newHazard.transform.position = bounds.center + new Vector3(offsetX, offsetY, offsetZ);
-            }
+            float offsetX = Random.Range(-bounds.extents.x, bounds.extents.x);
+            float offsetY = Random.Range(-bounds.extents.y, bounds.extents.y);
+            float offsetZ = Random.Range(-bounds.extents.z, bounds.extents.z);
+            //Instantiate(asteroid, Random.insideUnitSphere * radius + transform.position, Random.rotation);
+            GameObject newHazard = GameObject.Instantiate(asteroid);
+            newHazard.transform.position = bounds.center + new Vector3(offsetX, offsetY, offsetZ);
 
         }
     }
