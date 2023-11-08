364e6bd40c3cf32bd1345c4ba7467007ed3e41da
JonesCGD <80253281+gingaice@users.noreply.github.com>
2023-11-08 20:13:23 +0000

    spawner somewhat what i want

--- Assets/Code/ObstacleSpawner.cs
+++ Assets/Code/ObstacleSpawner.cs
@@ -1,29 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    BaseObstacle asteroid;
+    //BaseObstacle asteroid;
 
     [SerializeField]
-    float radius;
+    GameObject asteroid;
+
+    //[SerializeField]
+    //float radius;
 
     // Start is called before the first frame update
     void Start()
     {
-        asteroid = new BaseObstacle();
         StartSpawn();
     }
 
 
-    private void OnDrawGizmos()
-    {
-        //this is to get how far the ai can see
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, radius);
-    }
+    //private void OnDrawGizmos()
+    //{
+    //    //this is to get how far the ai can see
+    //    Gizmos.color = Color.yellow;
+    //    Gizmos.DrawWireSphere(transform.position, radius);
+    //}
     // Update is called once per frame
     void Update()
     {
@@ -32,13 +35,19 @@ public class ObstacleSpawner : MonoBehaviour
 
     void StartSpawn()
     {
-        
+        Bounds bounds = GetComponent<Collider>().bounds;
+
         if (asteroid != null) 
         {
             //Instantiate(asteroid, );
             for(int i = 0;i < 4;i++)
             {
-                Instantiate(asteroid, Random.insideUnitSphere * radius + transform.position, Random.rotation);
+                float offsetX = Random.Range(-bounds.extents.x, bounds.extents.x);
+                float offsetY = Random.Range(-bounds.extents.y, bounds.extents.y);
+                float offsetZ = Random.Range(-bounds.extents.z, bounds.extents.z);
+                //Instantiate(asteroid, Random.insideUnitSphere * radius + transform.position, Random.rotation);
+                GameObject newHazard = GameObject.Instantiate(asteroid);
+                newHazard.transform.position = bounds.center + new Vector3(offsetX, offsetY, offsetZ);
             }
 
         }
