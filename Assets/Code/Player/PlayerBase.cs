0f419d610c2657905d8a2bbf22f4223eb3cf98bb
DidEARobya <bench2001@hotmail.co.uk>
2023-11-09 18:47:41 +0000

    Area spawner and areas class

--- Assets/Code/Player/PlayerBase.cs
+++ Assets/Code/Player/PlayerBase.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerBase : MonoBehaviour
+public class PlayerBase : MonoBehaviour, IDamage
 {
     [SerializeField]
     public int ammo;
+
+    [SerializeField]
+    public int baseAttack;
+    private int attack;
+
     [SerializeField]
     public float pressureIncrement;
     [SerializeField]
@@ -16,6 +21,11 @@ public class PlayerBase : MonoBehaviour
     [SerializeField]
     public Sprite movingSprite;
 
+    [SerializeField]
+    private GameObject bullet;
+    [SerializeField]
+    public float shotCooldown;
+
     PlayerController controller;
     SpriteRenderer spriteRenderer;
 
@@ -25,20 +35,20 @@ public class PlayerBase : MonoBehaviour
     float pressureChangeTimer;
     float pressureChangeCooldown;
 
-    [SerializeField]
-    GameObject bullet;
-    [SerializeField]
-    Transform attackPos;
+
     AudioSource gunShot;
+
     [SerializeField]
     public float bulletForce;
 
-    bool allowInvoke=true;
+    bool canFire;
+
     // Start is called before the first frame update
     void Start()
     {
+        attack = baseAttack;
         durability = 100;
-        pressure = 0;
+        pressure = 100;
 
         pressureChangeTimer = 0;
         pressureChangeCooldown = 1;
@@ -46,6 +56,8 @@ public class PlayerBase : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
         controller = GetComponent<PlayerController>();
         gunShot = GetComponent<AudioSource>();
+
+        canFire = true;
     }
 
     // Update is called once per frame
@@ -64,31 +76,32 @@ public class PlayerBase : MonoBehaviour
 
         if (durability < 100)
         {
-            IncreasePressure();
+            DecreasePressure();
         }
         else
         {
-            if (pressure != 0)
+            if (pressure != 100)
             {
-                DecreasePressure();
+                IncreasePressure();
             }
         }
     }
 
     private void IncreasePressure()
     {
-        if(pressureChangeTimer < pressureChangeCooldown)
+        if (pressureChangeTimer < pressureChangeCooldown)
         {
             return;
         }
 
-        float damage = 100 - durability;
-
-        float pressureIncrease = damage * pressureIncrement;
-
-        float endPressure = pressure + pressureIncrease;
+        float endPressure = pressure + pressureIncrement;
 
         pressure = Mathf.Lerp(pressure, endPressure, 1);
+
+        if (pressure > 100)
+        {
+            pressure = 100;
+        }
 
         pressureChangeTimer = 0;
 
@@ -100,17 +113,17 @@ public class PlayerBase : MonoBehaviour
             return;
         }
 
-        float endPressure = pressure - pressureDecrement;
+        float damage = 100 - durability;
+
+        float pressureDecrease = damage * pressureDecrement;
+
+        float endPressure = pressure - pressureDecrease;
 
         pressure = Mathf.Lerp(pressure, endPressure, 1);
 
-        if (pressure < 0)
-        {
-            pressure = 0;
-        }
-
         pressureChangeTimer = 0;
     }
+
     public void TakeDamage(int damage)
     {
         durability -= damage;
@@ -125,20 +138,26 @@ public class PlayerBase : MonoBehaviour
     }
     public void Fire()
     {
-        Vector2 dir = transform.up;
-
-        GameObject projectile = Instantiate(bullet, transform.position, Quaternion.identity);
-        projectile.GetComponent<Rigidbody2D>().AddForce(dir.normalized * bulletForce, ForceMode2D.Impulse);
-        if(gunShot!=null)
-            gunShot.Play();
-        if(allowInvoke)
+        if(canFire == true)
         {
-            Invoke("ResetShot", 2.0f);
-            allowInvoke = false;
+            Vector2 dir = transform.up;
+
+            GameObject projectile = Instantiate(bullet, transform.position, Quaternion.identity);
+
+            projectile.GetComponent<Projectile>().Init(attack);
+            projectile.GetComponent<Rigidbody2D>().AddForce(dir.normalized * bulletForce, ForceMode2D.Impulse);
+
+            if (gunShot != null)
+            {
+                gunShot.Play();
+            }
+
+            Invoke("ResetShot", shotCooldown);
+            canFire = false;
         }
     }
     private void ResetShot()
     {
-        allowInvoke = true;
+        canFire = true;
     }    
 }
