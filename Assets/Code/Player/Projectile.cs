74e49b652916dc8495b78253b698c7af41c0dfa5
DidEARobya <bench2001@hotmail.co.uk>
2023-11-09 16:34:18 +0000

    Fixed bullets

--- Assets/Code/Player/Projectile.cs
+++ Assets/Code/Player/Projectile.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    [SerializeField] GameObject bullet;
+    [SerializeField] 
+    GameObject bullet;
+
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
