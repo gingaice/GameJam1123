3511b722f336863813c1f5936ca7554a6c259846
JonesCGD <80253281+gingaice@users.noreply.github.com>
2023-11-09 15:07:05 +0000

    ok pause menu now work

--- Assets/Code/Menus/SceneChanger.cs
+++ Assets/Code/Menus/SceneChanger.cs
@@ -6,8 +6,13 @@ using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
    public void ChangeScene(string sceneName)
-    {
+   {
+        Time.timeScale = 1;
         SceneManager.LoadScene(sceneName);
+   }
+    public void Resume()
+    {
+        Time.timeScale = 1;
     }
     public void Exit()
     {
