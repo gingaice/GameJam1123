25b64f5cab23efcec87cee6556cc5aab301b2c8c
DidEARobya <bench2001@hotmail.co.uk>
2023-11-09 21:51:05 +0000

    Fixed Miniboss

--- Assets/Code/Enemies/MiniBoss.cs
+++ Assets/Code/Enemies/MiniBoss.cs
@@ -1,29 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MiniBoss : EnemyBase
 {
+    [SerializeField]
+    private GameObject bullet;
+    [SerializeField]
+    public float shotCooldown;
+    [SerializeField]
+    public float bulletForce;
+    [SerializeField]
+    public float fireRange;
 
-    PlayerBase player;
-    private void OnCollisionEnter2D(Collision2D collision)
+    [SerializeField]
+    public float meleeCooldown;
+    private float meleeTimer;
+
+    bool canFire;
+
+    private void Start()
     {
-        if(collision.collider==player)
+        base.Start();
+
+        canFire = true;
+        meleeTimer = 0;
+    }
+    private new void Update()
+    {
+        meleeTimer += Time.deltaTime;
+
+        Vector2 distance = player.transform.position - transform.position;
+
+        if(distance.magnitude <= fireRange)
         {
-             player.TakeDamage(attack);
+            Fire();
         }
-     
+
+        base.Update();
     }
-    // Start is called before the first frame update
-    void Start()
+    public void Fire()
     {
-        player = GetComponent<PlayerBase>();
+        if (canFire == true)
+        {
+            Vector2 dir = player.transform.position - transform.position;
+
+            GameObject projectile = Instantiate(bullet, transform.position, Quaternion.identity);
+
+            projectile.GetComponent<Projectile>().Init(attack);
+            projectile.GetComponent<Rigidbody2D>().AddForce(dir.normalized * bulletForce, ForceMode2D.Impulse);
+
+
+            Invoke("ResetShot", shotCooldown);
+            canFire = false;
+        }
+    }
+    private void ResetShot()
+    {
+        canFire = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private new void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if(collision.collider.gameObject == player)
+        {
+            if(meleeTimer >= meleeCooldown)
+            {
+                player.GetComponent<PlayerBase>().TakeDamage(attack);
+
+                meleeTimer = 0;
+            }
+        }
     }
  
 }
